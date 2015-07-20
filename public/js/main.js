1b73e913567b9c69bc831e6e64ded5bf50f0696d
John DeWolf <jdewolf@backupify.com>
2015-07-20 18:04:33 -0400

    replace table with UL

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,15 @@
-var button = document.getElementById('runButton');
-var combyTable = document.getElementById('results');
+var resultList = document.getElementById('results');
 
-if (combyTable.childNodes[1].childElementCount > 0) {
+if (resultList.childElementCount > 0) {
 	document.getElementById("combsays").style.display = '';
+	Materialize.showStaggeredList('#staggered-test');
 }
 
-button.addEventListener('click', function() {
-
-	while ( combyTable.childNodes[1].firstChild ) {
-		 combyTable.childNodes[1].removeChild(combyTable.childNodes[1].firstChild);
-	}
+document.getElementById('runButton').addEventListener('mousedown', function() {
+	document.getElementById("combsays").style.display = '';
+	document.querySelector(".preloader-wrapper").style.display = '';
 
 	var firstName = document.querySelector('form').first_name.value;
 	var lastName = document.querySelector('form').last_name.value;
 	var domain = document.querySelector('form').domain.value;
-
-	if (firstName && lastName && domain ) {
-		document.getElementById("combsays").style.display = '';
-		document.querySelector(".preloader-wrapper").style.display = '';
-	}
 });
