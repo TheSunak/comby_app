fed4caf50d38ec5c862cd403092e9daabbce095d
Vishal Sunak <vishal@backupify.com>
2015-07-20 15:43:13 -0400

    Comby styled

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+var button = document.getElementById('runButton');
+
+button.addEventListener('mousedown', function(event) {
+	var combyTable = document.getElementById('results');
+	document.querySelector(".combsays").style.display = 'block';
+	if (results.childElementCount === 1) {
+		document.querySelector(".preloader-wrapper").style.display = 'block';
+	} 
+
+		
+	
+});
